ad56fe30698aea005cc7866b57ad851ca30d9a8d
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-05-09 22:29:21 +0500

    Change button's text to "Read more"

--- src/components/main-content/IndexData.jsx
+++ src/components/main-content/IndexData.jsx
@@ -9,7 +9,7 @@ function IndexData() {
     <h2 className="data-content__title title">We professional gardener!</h2>
     <p className="data-content__description">Morbi magna metus, scelerisque at risus a, tristique laoreet orci. Duis posuere eros vel dui pulvinar, pulvinar molestie erat iaculis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia.</p>
     <div className="arrow-button-wrapper">
-    <button className="data-content__button button button--arrow">Shop now</button>
+    <button className="data-content__button button button--arrow">Read more</button>
     </div>
     </div>
     <img className="index-data__img" src={GardenerGril} alt="A gardener gril" />
