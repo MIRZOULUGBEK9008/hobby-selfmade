71f17cca68474bd3ec2bdca0a1a97f8bfdac406b
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-05-09 21:58:11 +0500

    Add section index-data. Add modifier class to main-content

--- src/components/main-content/MainContent.jsx
+++ src/components/main-content/MainContent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import IndexStatistic from './IndexStatistic'
+import IndexData from './IndexData'
 
 function MainContent() {
   return (
-    <main className='main-content'>
+    <main className='main-content main-content--blur'>
     <IndexStatistic/>
+    <IndexData/>
     </main>
     )
   }
