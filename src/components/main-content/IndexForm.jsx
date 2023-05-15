267cafc90e63c3d8a944d8f4fda75df81e3c4d93
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-05-15 11:34:38 +0500

    Add form

--- src/components/main-content/IndexForm.jsx
+++ src/components/main-content/IndexForm.jsx
@@ -4,17 +4,18 @@ const IndexForm = () => {
   return (
     <section className="index-form">
     <div className="index-form__container container">
-    <div className="index-form__content">
-    <h2 className="index-form__title title">Ready to bring idea for your garden?</h2>
-    <p className="index-form__description">I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete</p>
+    <div className="index-form-content-form-wrapper">
+      <div className="index-form__content">
+      <h2 className="index-form__title title">Ready to bring idea for your garden?</h2>
+      <p className="index-form__description">I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete</p>
+      </div>
+      <form className="index-form__form form-index" action="https://echo.htmlacademy.ru/" method="GET" target="blank">
+      <div className="form-index__inner">
+      <input className="form-index__input" type="email" placeholder="Enter your email" name="email" required/>
+      <button className="form-index__button button button--green-small button--green-big">Subscribe</button>
+      </div>
+      </form>
     </div>
-
-    <form className="index-form__form form-index" action="https://echo.htmlacademy.ru/" method="GET" target="blank">
-    <div className="form-index__inner">
-    <input className="form-index__input" type="email" placeholder="Enter your email" name="email" required/>
-    <button className="form-index__button button button--green-small button--green-big">Subscribe</button>
-    </div>
-    </form>
     </div>
     </section>
     )
